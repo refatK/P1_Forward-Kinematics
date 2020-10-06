39f472df9df204d0acae67e328b4ac758821dda8
Refat Kamal <refat.k17@gmail.com>
2020-10-06 00:55:47 -0400

    Added simple transforms based on parent postion for nodes

--- solution/a1solution.cpp
+++ solution/a1solution.cpp
@@ -9,6 +9,7 @@ A1Solution::A1Solution(std::vector<Joint2D*>& joints, std::vector<Link2D*>& link
     :m_joints(joints),
     m_links(links){
 
+    this->initialize_joint_transforms();
 }
 
 
@@ -16,7 +17,30 @@ A1Solution::A1Solution(std::vector<Joint2D*>& joints, std::vector<Link2D*>& link
 void A1Solution::update(Joint2D* selected, QVector2D mouse_pos){
 
     // Students implement solution here
+}
 
+void A1Solution::initialize_joint_transform(int i) {
+    Joint2D* joint = m_joints[i];
+    QVector2D joint_pos = joint->get_position();
+
+    if(joint->get_parents().empty()) {
+        // Use relative postion if child
+        QVector2D parent_pos = joint->get_parents()[0]->get_position();
+        QVector2D relative_pos = joint_pos - parent_pos;
+        this->m_joint_transforms[i] = Eigen::Vector2f(relative_pos.x(), relative_pos.y());
+
+    } else {
+        // Use global postion if root
+        this->m_joint_transforms[i] = Eigen::Vector2f(joint_pos.x(), joint_pos.y());
+    }
+
+    return;
+}
+
+void A1Solution::initialize_joint_transforms() {
+    for(unsigned int i=0; i < this->m_joints.size(); ++i){
+        this->initialize_joint_transform(i);
+    }
 }
 
 
--- solution/a1solution.h
+++ solution/a1solution.h
@@ -2,6 +2,7 @@
 #define A1SOLUTION_H
 
 #include <vector>
+#include <dependencies/Eigen/Dense>
 
 #include "OpenGL/elements/joint2D.h"
 #include "OpenGL/elements/obstacle2d.h"
@@ -15,11 +16,16 @@ public:
     std::vector<Joint2D*>& m_joints;
     std::vector<Link2D*>& m_links;
 
+    std::vector<Eigen::Vector2f> m_joint_transforms;
+
 
     void update(Joint2D* selected, QVector2D mouse_pos);
 
-
     static void test_eigen_library();
+
+private:
+    void initialize_joint_transform(int index);
+    void initialize_joint_transforms();
 
 };
 
